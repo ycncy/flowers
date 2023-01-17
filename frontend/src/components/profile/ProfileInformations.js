be6def2e0993f4fd1a23e2a8a6468458f3b5e654
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-01-17 13:34:41 +0100

    Jpp des bugs

--- frontend/src/components/profile/ProfileInformations.js
+++ frontend/src/components/profile/ProfileInformations.js
@@ -1,29 +1,7 @@
-import React, {useEffect, useState} from 'react';
-
-import Publications from "../posts/Publications";
-import Followers from "../posts/Followers";
-import Following from "../posts/Following";
-
-const ProfileInformations = (props) => {
+const ProfileInformations = () => {
 
     return (
         <div>
-            <nav>
-                <button onClick={() => showHide("publications")}>Publications
-                </button>
-                <button onClick={() => showHide("followers")}>Abonnés</button>
-                <button onClick={() => showHide("following")}>Abonnements
-                </button>
-            </nav>
-            <div className="content">
-                <hr/>
-                {divStates.publications &&
-                    <Publications username={props.username}/>}
-                <hr/>
-                {divStates.followers && <Followers/>}
-                <hr/>
-                {divStates.following && <Following/>}
-            </div>
         </div>
     );
 };
